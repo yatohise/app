5c815fd82f8e7951cebdb8ba0e435b5efedbec18
長澤　友也 <tomoaya@nagasawatomoyanoMacBook-Air.local>
2020-05-21 17:58:06 +0900

    Add message

--- db/migrate/20200521002906_create_messages.rb
+++ db/migrate/20200521002906_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :title
-      t.string :maesssage
+      t.string :message
 
       t.timestamps
     end
